256582ce8b3e73d0b0bf048a3e262e697513cd7a
Nicolas The Boss <noudard+theboss@weborama.com>
2015-07-05 14:38:52 +0200

    Add Display package

--- twitterpage.py
+++ twitterpage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
